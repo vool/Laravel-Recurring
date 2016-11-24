e97238cdb4c4d9c81842edccf4f510bbd298a551
Brian Faust <faustbrian@users.noreply.github.com>
2016-11-24 13:08:18 +0200

    Applied fixes from StyleCI (#1)
    [ci skip] [skip ci]

--- src/Config.php
+++ src/Config.php
@@ -1,9 +1,18 @@
 <?php
 
+/*
+ * This file is part of Laravel Recurring.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Recurring;
 
-use Recurr\Frequency;
 use Illuminate\Contracts\Support\Arrayable;
+use Recurr\Frequency;
 
 class Config implements Arrayable
 {
